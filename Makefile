169da8db2911ddf25c1056ec6ca60740b5c1799d
petkovic <vladan.petkovic27@gmail.com>
2024-11-26 22:55:01 +0100

    updated makefile

--- Makefile
+++ Makefile
@@ -4,8 +4,6 @@ GHC_FLAGS = -package containers -outputdir bin
 BIN_DIR = bin
 
 SRC_FILES = src/Main.hs src/RBTree.hs src/Helperfunctions.hs
-TEST_FILES = scr/TestHelperfunctions.hs
-TEST_HELPER_EXEC = $(BIN_DIR)/test
 EXEC = $(BIN_DIR)/sort
 
 .PHONY: all clean run
@@ -17,13 +15,6 @@ $(EXEC): $(SRC_FILES)
 	@echo "Compiling Haskell program..."
 	mkdir -p $(BIN_DIR)
 	$(GHC) $(SRC_FILES) $(GHC_FLAGS) -o $(EXEC)
-
-test: $(TEST_FILES)
-	@echo "Compiling TestHelperfunctions..."
-	mkdir -p $(BIN_DIR)
-	$(GHC) $(TEST_FILES) $(SRC_FILES) $(GHC_FLAGS) -o $(TEST_HELPER_EXEC)
-	@echo "Running TestHelperfunctions..."
-	./$(TEST_HELPER_EXEC)
 
 clean:
 	@echo "Cleaning up..."
--- src/TestHelperfunctions.hs
+++ src/TestHelperfunctions.hs
@@ -4,34 +4,20 @@ import Helperfunctions ( parallelTokenize, tokenize )
 import Test.QuickCheck ( quickCheck )
 import Data.Char (isLower)
 
--- Property 1: Tokenized output contains only lowercase words
 prop_tokenizeLowercase :: String -> Bool
 prop_tokenizeLowercase content =
     all (all isLower) (tokenize content)
 
--- Property 2: Tokenized output contains no punctuation or empty words
 prop_tokenizeNoPunctuation :: String -> Bool
 prop_tokenizeNoPunctuation content =
     all (not . null) (tokenize content)
 
--- Property 3: Parallel tokenization matches sequential tokenization
 prop_parallelTokenizeSameAsSequential :: String -> Bool
 prop_parallelTokenizeSameAsSequential content =
     parallelTokenize content == concatMap tokenize (lines content)
 
--- Property 4: Sorted words remain sorted after writing
-prop_writeSortedWordsSorted :: [String] -> Bool
-prop_writeSortedWordsSorted words =
-    let sortedWords = quicksort words
-    in sortedWords == sortedWords
-  where
-    quicksort [] = []
-    quicksort (x:xs) = quicksort [y | y <- xs, y <= x] ++ [x] ++ quicksort [y | y <- xs, y > x]
-
--- Define the main function to run tests
 main :: IO ()
 main = do
     quickCheck prop_tokenizeLowercase
     quickCheck prop_tokenizeNoPunctuation
     quickCheck prop_parallelTokenizeSameAsSequential
-    quickCheck prop_writeSortedWordsSorted
